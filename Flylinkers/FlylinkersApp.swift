ae67360ba217ce37befae5ffe95b8190bc2f8388
Omar-Lamprea <lampreaomar@gmail.com>
2022-05-13 14:58:42 -0500

    Initial Commit

---
+++ Flylinkers/FlylinkersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlylinkersApp.swift
+//  Flylinkers
+//
+//  Created by Omar Lamprea on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FlylinkersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
